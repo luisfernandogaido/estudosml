ae1c8bf89e60eaa151858f81891930f60452479d
Luis Fernando Gaido <luisfernandogaido@gmail.com>
2018-09-07 01:30:28 -0300

    determinante de matriz de qualquer ordem

--- main.go
+++ main.go
@@ -5,23 +5,25 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/luisfernandogaido/estudosml/matriz"
 )
 
 func main() {
-	a := matriz.New([][]float64{
-		{2, 5, 9},
-		{3, 6, 8},
+	t0 := time.Now()
+	m := matriz.New([][]float64{
+		{1, 2, 2, -1, 2, 1},
+		{3, 1, -2, 4, -2, 2},
+		{-5, 0, 7, 1, 8, 1},
+		{1, 2, 3, 4, -6, -3},
+		{2, 2, -1, 3, 5, 6},
+		{9, -7, 2, 1, 0, -3},
 	})
-	b := matriz.New([][]float64{
-		{2, 7},
-		{4, 3},
-		{5, 2},
-	})
-	c, err := a.Multiplica(b)
+	det, err := m.Det()
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(c)
+	fmt.Println(det)
+	fmt.Println(time.Since(t0))
 }
--- matriz/matriz.go
+++ matriz/matriz.go
@@ -3,6 +3,7 @@ package matriz
 import (
 	"errors"
 	"fmt"
+	"math"
 )
 
 type Matriz struct {
@@ -11,6 +12,10 @@ type Matriz struct {
 
 func New(v [][]float64) Matriz {
 	return Matriz{v}
+}
+
+func (m Matriz) Dim() (int, int) {
+	return len(m.elementos), len(m.elementos[0])
 }
 
 func (m Matriz) Em(i, j int) float64 {
@@ -80,4 +85,36 @@ func (m Matriz) Multiplica(m2 Matriz) (Matriz, error) {
 		valores = append(valores, linha)
 	}
 	return Matriz{valores}, nil
+}
+
+func (m Matriz) Det() (float64, error) {
+	M, N := m.Dim()
+	if M != N {
+		return 0, errors.New("matriz precisa ser quadrada para seu determinante ser calculado")
+	}
+	if N == 2 {
+		d := m.elementos[0][0]*m.elementos[1][1] - m.elementos[0][1]*m.elementos[1][0]
+		return d, nil
+	}
+	d := 0.0
+	for k := 0; k < N; k++ {
+		valoresMenor := make([][]float64, 0)
+		for i := 0; i < N; i++ {
+			if i == k {
+				continue
+			}
+			linha := make([]float64, 0)
+			for j := 1; j < N; j++ {
+				linha = append(linha, m.elementos[i][j])
+			}
+			valoresMenor = append(valoresMenor, linha)
+		}
+		menor := Matriz{valoresMenor}
+		dMenor, err := menor.Det()
+		if err != nil {
+			return 0, err
+		}
+		d += math.Pow(-1, float64(k)+2) * m.elementos[k][0] * dMenor
+	}
+	return d, nil
 }
